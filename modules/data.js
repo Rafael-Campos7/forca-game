cb86e85292568e2489ceef0fb0b63932bceb9d44
Rafael Campos <rafaelscampos15@gmail.com>
2021-02-18 12:37:24 -0300

    Created the application database.

---
+++ modules/data.js
@@ -0,0 +1,14 @@
+let words = [
+    { word: "carro", tip:"Meio de Transporte" },
+    { word: "banana", tip:"Fruta" },
+    { word: "abacate", tip: "Fruta" },
+    { word: "amarelo", tip: "Cor" },
+    { word: "avião", tip: "Meio de Tranporte" },
+    { word: "girafa", tip: "Animal" },
+    { word: "parque", tip: "Lugar"},
+    { word: "elefante", tip: "Animal"},
+    { word: "celular", tip: "Dispositivo"},
+    { word: "limonada", tip: "Bebida"},
+]
+
+export default words;
