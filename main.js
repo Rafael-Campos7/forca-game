0b3c07420cfff8b7cbc5868f3056ea71f93bf296
Rafael Campos <rafaelscampos15@gmail.com>
2021-02-21 09:34:13 -0300

    Modified main.js and created the initial instructions.

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 import gameEngine from "./modules/logic.js"
 import domManipulation from "./modules/interface.js"
 
-gameEngine.wordSelector()
-domManipulation.wordGenerator()
+function onStarted() {
+    gameEngine.wordSelector()
+    domManipulation.addTip(gameEngine.selectedTip)
+    gameEngine.gameInstructions(1)
+    domManipulation.displaysWord()
+}
+
+function onTryed() {
+    
+}
+
+function onAnswered() {
+
+}
+
+document.onload = onStarted()
