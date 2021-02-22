5e6991d69995d76960af30e638f2496263c713f6
Rafael Campos <rafaelscampos15@gmail.com>
2021-02-22 09:49:39 -0300

    Created validations.js and declared the attempted letter validations.

---
+++ modules/validations.js
@@ -0,0 +1,25 @@
+let tryValidations = {
+    alreadyInserted: function(inputLetter,trieds) {
+        let result = false
+        const check = (value) => {
+            if (inputLetter == value) {
+                result = true
+            }
+        }
+        trieds.forEach(check)
+        return result
+    },
+    letterCheck: function(inputLetter, word) {
+        let result = [false]
+        for (let index in word) {
+            if (word[index] == inputLetter) {
+                result[0] = true
+                result.push(index)
+            }
+        }
+        return result
+    }
+
+}
+
+export default tryValidations
